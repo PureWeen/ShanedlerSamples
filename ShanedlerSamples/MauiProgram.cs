f6109b5cbb5d5c8c3623cec1bd6925a922db2432
Shane Neuville <shneuvil@microsoft.com>
2022-09-13 17:37:22 -0500

    Add material handler

--- ShanedlerSamples/MauiProgram.cs
+++ ShanedlerSamples/MauiProgram.cs
@@ -1,18 +1,26 @@
-﻿namespace ShanedlerSamples;
+﻿using Microsoft.Maui.Hosting;
+
+namespace ShanedlerSamples;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureMauiHandlers(handlers =>
+            {
+#if ANDROID
+                handlers.AddHandler<Entry, ShanedlerSamples.Platforms.Android.MaterialEntryHandler>();
+#endif
+            })
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
