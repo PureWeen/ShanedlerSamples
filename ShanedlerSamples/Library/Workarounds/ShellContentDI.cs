69766cf7e9311ad71f635cf334071f467b026336
Shane Neuville <shneuvil@microsoft.com>
2023-02-04 19:26:56 -0600

    - check if already created

--- ShanedlerSamples/Library/Workarounds/ShellContentDI.cs
+++ ShanedlerSamples/Library/Workarounds/ShellContentDI.cs
@@ -8,8 +8,20 @@ namespace ShanedlerSamples
 {
     public class ShellContentDI : ShellContent, IShellContentController
     {
+        bool created = false;
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            created = false;
+        }
+
         Page IShellContentController.GetOrCreateContent()
         {
+            if (created && Content is Page createdPage)
+                return createdPage;
+
+            created = true;
             var page = (Page)Routing.GetOrCreateContent(this.Route, Parent.Parent.Parent.Handler.MauiContext.Services);
             Content = page;
             return page;
