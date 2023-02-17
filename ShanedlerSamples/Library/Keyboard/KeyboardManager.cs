406a6792d04d91942c8183751402ac95f19ed1bc
Shane Neuville <shneuvil@microsoft.com>
2023-02-16 18:11:32 -0600

    - focus an element if it's not focused before opening keyboard

--- ShanedlerSamples/Library/Keyboard/KeyboardManager.cs
+++ ShanedlerSamples/Library/Keyboard/KeyboardManager.cs
@@ -7,8 +7,19 @@ namespace Shanedler.Workarounds
         public static void HideKeyboard(this IView inputView) =>
             ((IPlatformViewHandler)inputView.Handler).PlatformView.HideKeyboard();
 
-        public static void ShowKeyboard(this IView inputView) =>
-            ((IPlatformViewHandler)inputView.Handler).PlatformView.ShowKeyboard();
+        public static void ShowKeyboard(this IView inputView)
+        {
+            if (!inputView.IsFocused)
+            {
+                (inputView as VisualElement).Focus();
+                (inputView as VisualElement).Dispatcher.Dispatch(() =>
+                {
+                    ((IPlatformViewHandler)inputView.Handler).PlatformView.ShowKeyboard();
+                });
+            }
+            else
+                ((IPlatformViewHandler)inputView.Handler).PlatformView.ShowKeyboard();
+        }
 
         public static bool IsSoftKeyboardVisible(this IView view) =>
             ((IPlatformViewHandler)view.Handler).PlatformView.IsSoftKeyboardVisible();
