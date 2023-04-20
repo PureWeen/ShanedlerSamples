3d098d25ef8f74bc5786e61860a2855e0ba03316
Shane Neuville <shneuvil@microsoft.com>
2023-04-19 19:48:07 -0500

    input transparent workaround

--- ShanedlerSamples/Library/Common/HostExtensions.cs
+++ ShanedlerSamples/Library/Common/HostExtensions.cs
@@ -10,6 +10,64 @@ namespace Maui.FixesAndWorkarounds
 {
 	public static partial class HostExtensions
 	{
+		public static MauiAppBuilder ConfigureInputTransparentFixes(this MauiAppBuilder builder)
+		{
+			builder.ConfigureMauiHandlers(_ =>
+			{
+				ViewHandler.ViewMapper.ModifyMapping(nameof(ViewHandler.ContainerView), (handler, view, action) =>
+				{
+					action.Invoke(handler, view);
+
+#if ANDROID
+					if (handler.ContainerView is Microsoft.Maui.Platform.WrapperView wrapper)
+						wrapper.InputTransparent = false;
+#endif
+				});
+
+				ViewHandler.ViewMapper.ModifyMapping(nameof(IView.InputTransparent), (handler, view, action) =>
+				{
+					action.Invoke(handler, view);
+#if WINDOWS
+					if (handler.PlatformView is Microsoft.Maui.Platform.LayoutPanel lp)
+					{
+						lp.IsHitTestVisible = true;
+					}
+#endif
+
+					if (handler is ILayoutHandler && view is Microsoft.Maui.ILayout)
+					{
+
+					}
+					else
+					{
+						VisualElement.ControlsVisualElementMapper.UpdateProperty(handler, view, nameof(IView.InputTransparent));
+					}
+				});
+
+				ViewHandler.ViewMapper.ModifyMapping(nameof(Layout.CascadeInputTransparent), (handler, view, action) =>
+				{
+					action.Invoke(handler, view);
+#if WINDOWS
+					if (handler.PlatformView is Microsoft.Maui.Platform.LayoutPanel lp)
+					{
+						lp.IsHitTestVisible = true;
+					}
+#endif
+
+					if (handler is ILayoutHandler && view is Microsoft.Maui.ILayout)
+					{
+
+					}
+					else
+					{
+						VisualElement.ControlsVisualElementMapper.UpdateProperty(handler, view, nameof(Layout.CascadeInputTransparent));
+					}
+				});
+			});
+
+			return builder.ConfigureMauiWorkarounds(true);
+		}
+
 		public static MauiAppBuilder ConfigureMauiWorkarounds(this MauiAppBuilder builder)
 		{
 			return builder.ConfigureMauiWorkarounds(true);
@@ -30,6 +88,7 @@ namespace Maui.FixesAndWorkarounds
 
 			if (addAllWorkaround)
 			{
+				builder.ConfigureInputTransparentFixes();
 				builder.ConfigureShellWorkarounds();
 				builder.ConfigureTabbedPageWorkarounds();
 				builder.ConfigureEntryNextWorkaround();
