4a887b79988f68b463d363819788858f8aef2a10
Shane Neuville <shneuvil@microsoft.com>
2023-08-01 11:47:23 -0500

    Quick keyboard samples

--- ShanedlerSamples/App.xaml.cs
+++ ShanedlerSamples/App.xaml.cs
@@ -5,7 +5,55 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
+	}
 
-		MainPage = new AppShell();
+	protected override Window CreateWindow(IActivationState activationState)
+	{
+		return new KeyboardCapableWindow(new AppShell());
+	}
+
+
+	class KeyboardCapableWindow : Window
+	{
+		public KeyboardCapableWindow()
+		{
+		}
+
+		public KeyboardCapableWindow(Page page) : base(page)
+		{
+			page.HandlerChanged += OnPageHandlerChanged;
+		}
+
+		void OnPageHandlerChanged(object sender, EventArgs e)
+		{
+
+			if (sender is IView view && view.Handler is IPlatformViewHandler pvh)
+			{
+#if WINDOWS
+				pvh.PlatformView.KeyDown += (X, Y) =>
+				{
+
+				};
+				pvh.PlatformView.KeyUp += (X, Y) =>
+				{
+
+				};
+				pvh.PlatformView.PreviewKeyDown += (X, Y) =>
+				{
+
+				};
+
+				pvh.PlatformView.PreviewKeyUp += (X, Y) =>
+				{
+
+				};
+#endif
+			}
+		}
+
+		protected override void OnHandlerChanged()
+		{
+			base.OnHandlerChanged();
+		}
 	}
 }
